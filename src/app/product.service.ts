fc5147fde15c2d960cddacffe80bdc09067d711f
tobeoct <tobe.onyema@gmail.com>
2019-11-03 15:49:59 +0100

    Fixes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
-  _albumUrl:string = '../assets/album.json';
+  private _albumUrl:string = '../assets/album.json';
   constructor(private _http:Http) { }
 getAlbum(id:number){
   return this._http.get(this._albumUrl).map((response)=>
